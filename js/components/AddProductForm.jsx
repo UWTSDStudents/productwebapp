d84cfff2eb7d88a10b331f73ec4ae133669d054e
Mike Dacey <michael.dacey@uwtsd.ac.uk>
2023-07-31 15:39:50 +0100

    Modified to clear message on adding next product

--- js/components/AddProductForm.jsx
+++ js/components/AddProductForm.jsx
@@ -28,15 +28,15 @@ function AddProductForm(props) {
         <form id='add-product-form' onSubmit={handleSubmit}>
             <div className="mb-3">
             <label htmlFor="productName" className="form-label">Product Name</label>
-            <input type="text" className="form-control" id="productName" name="productName" required value={name} onChange={(event)=>setName(event.target.value)} />
+            <input type="text" className="form-control" id="productName" name="productName" required value={name} onChange={(event)=>{setName(event.target.value);setMessage(null);}} />
             </div>
             <div className="mb-3">
             <label htmlFor="productPrice" className="form-label">Price</label>
-            <input type="number" className="form-control" id="productPrice" name="productPrice" step="0.01" required value={price} onChange={(event)=>setPrice(event.target.value)} />
+            <input type="number" className="form-control" id="productPrice" name="productPrice" step="0.01" required value={price} onChange={(event)=>{setPrice(event.target.value);setMessage(null);}} />
             </div>
             <div className="mb-3">
             <label htmlFor="productDescription" className="form-label">Description</label>
-            <textarea className="form-control" id="productDescription" name="productDescription" rows="4" required value={description} onChange={(event)=>setDescription(event.target.value)} ></textarea>
+            <textarea className="form-control" id="productDescription" name="productDescription" rows="4" required value={description} onChange={(event)=>{setDescription(event.target.value);setMessage(null);}} ></textarea>
             </div>
             <button type="submit" className="btn btn-primary">Add Product</button>
             {/* Conditionally render the success/error message */}
